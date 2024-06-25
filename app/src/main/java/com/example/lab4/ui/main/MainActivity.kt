d5f28d57a6afaaa8fc2564a380e6ef94bee82e41
DESKTOP-K0TE4AO\sebas <sebastiangarciapachano@gmail.com>
2024-06-25 08:54:52 +0200

    toolbar and activity main layout

---
+++ app/src/main/java/com/example/lab4/ui/main/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.example.lab4.ui.main
+
+import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import com.example.lab4.R
+import com.example.lab4.databinding.ActivityMainBinding
+import com.example.lab4.ui.base.BaseActivity
+
+class MainActivity : BaseActivity<ActivityMainBinding>() {
+    override fun inflateBinding() {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun observeViewModel() = Unit
+
+    override fun createAfterInflateBindingSetupObserverViewModel(savedInstanceState: Bundle?) {
+        configNavigation()
+    }
+
+    override fun configureToolbarAndConfigScreenSections() = Unit
+
+    private fun configNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.mainNavHostFragment) as NavHostFragment
+        navController = navHostFragment.navController
+    }
+}
