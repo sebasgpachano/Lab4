ac5fb46527a41d55d65884ae485112df7fa87638
DESKTOP-K0TE4AO\sebas <sebastiangarciapachano@gmail.com>
2024-07-07 19:01:30 +0200

    dialog fragment for deleteUsers

--- app/src/main/java/com/example/lab4/ui/base/BaseFragment.kt
+++ app/src/main/java/com/example/lab4/ui/base/BaseFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.viewbinding.ViewBinding
+import com.example.lab4.R
+import com.example.lab4.ui.dialogfragment.ConfirmDeleteDialogFragment
 
 abstract class BaseFragment<B : ViewBinding> : Fragment() {
 
@@ -83,6 +85,11 @@ abstract class BaseFragment<B : ViewBinding> : Fragment() {
 
     fun hideKeyboard() {
         baseActivity.hideKeyboard()
+    }
+
+    fun showConfirmDeleteDialog(position: Int) {
+        val dialog = ConfirmDeleteDialogFragment.newInstance(position)
+        dialog.show(childFragmentManager, getString(R.string.confirm_delete_dialog_tag))
     }
 
     abstract fun inflateBinding()
